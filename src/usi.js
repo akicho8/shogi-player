acd1482a24e14213dd902f7bd12a7a5d00dd7368
akicho8 <akicho8@gmail.com>
2018-01-03 18:09:59 +0900

    sfen の moves 部分のパース

--- src/usi.js
+++ src/usi.js
@@ -1,18 +1,19 @@
-// -*- coding: utf-8; compile-command: "../node_modules/.bin/babel-node usi.js" -*-
-// -*- coding: utf-8; compile-command: "node usi.js" -*-
+// -*- compile-command: "babel-node usi.js" -*-
 
 var XRegExp = require('xregexp')
 // import { XRegExp } from 'xregexp'
 // const Piece = require('./piece')
 // import { XRegExp } from "xregexp"
 import { Piece } from './piece'
+import { Board } from './board'
+import { Point } from './point'
 
 // console.log(Piece.foo())
 
 class Sfen {
   constructor (source) {
     this.source = source
-    this.attributes = {}
+    this.attributes = null
   }
 
   parse () {
@@ -20,27 +21,24 @@ class Sfen {
     // (/position\s+(sfen\s+(?<sfen>\S+)\s+(?<b_or_w>\S+)\s+(?<hold_pieces>\S+)\s+(?<turn_counter_next>\d+)|(?<startpos>startpos))(\s+moves\s+(?<moves>.*))?/)
     const regex = XRegExp("sfen\\s+(?<sfen>\\S+)\\s+(?<b_or_w>\\S+)\\s+(?<hold_pieces>\\S+)\\s+(?<turn_counter_next>\\d+)(\\s+moves\\s+(?<moves>.*))?")
     this.md = XRegExp.exec(this.source, regex)
-    this.attributes        = this.md
-    this.sfen              = this.md["sfen"]
-    this.b_or_w            = this.md["b_or_w"]
-    this.turn_counter_next = this.md["turn_counter_next"]
-    this.moves             = this.md["moves"]
+    this.attributes = this.md
   }
 
-  field () {
+  initial_field () {
     let field = {}
-    this.sfen.split("/").forEach((e, y) => {
+    this.attributes["sfen"].split("/").forEach((e, y) => {
       let x = 0
       XRegExp.forEach(e, XRegExp("(?<promoted>\\+?)(?<piece>\\S)"), (m, i) => {
         if (/\d+/.test(m.piece)) {
           x += Number(m.piece)
         } else {
-          field[[x, y]] = {
-            position: {x: x, y: y},
+          let battler = {
+            pos: new Point([x, y]),
             piece: Piece.fetch(m.piece),
             promoted: (m.promoted === '+'),
             location: this.location_by(m.piece),
           }
+          field[battler.pos] = battler
           x += 1
         }
       })
@@ -57,7 +55,7 @@ class Sfen {
   }
 
   location () {
-    if (this.b_or_w === "b") {
+    if (this.attributes["b_or_w"] === "b") {
       return "black"
     } else {
       return "white"
@@ -80,17 +78,49 @@ class Sfen {
     return list
   }
 
-  turn_counter() {
-    return Number(this.attributes["turn_counter_next"]) - 1
+  turn_counter_next() {
+    return Number(this.attributes["turn_counter_next"])
+  }
+
+  turn_counter_base() {
+    return this.turn_counter_next() - 1
+  }
+
+  turn_counter_max() {
+    return this.turn_counter_base() + this.move_infos().length
   }
 
   komaochi_p() {
-    return (this.turn_counter() % 2) === 0 && this.location() == "white"
+    return (this.turn_counter_next() % 2) === 1 && this.location() == "white"
+  }
+
+  location_of(offset) {
+    const index = this.turn_counter_next() + offset
+    let retval = null
+    if ((index % 2) === 1) {
+      retval = "black"
+    } else {
+      retval = "white"
+    }
+    return retval
   }
 
   move_infos () {
-    return this.attributes["moves"].split(/\s+/).map((e) => {
-      return e
+    return this.attributes["moves"].split(/\s+/).map((e, i) => {
+      const attrs = {}
+      attrs["scene_index"] = this.turn_counter_base() + i
+      attrs["scene_offsert"] = i
+      attrs["location"] = this.location_of(i)
+
+      const md = XRegExp.exec(e, XRegExp("(?<origin_x>\\S)(?<origin_y>\\S)(?<pos_x>\\S)(?<pos_y>\\S)(?<promoted>\\+?)?"))
+      attrs["promoted_trigger"] = (md.promoted === "+")
+      if (md["origin_y"] === "*") {
+        attrs["stroke_piece"] = Piece.fetch(md["origin_x"])
+      } else {
+        attrs["origin_pos"] = new Point(`${md["origin_x"]}${md["origin_y"]}`)
+      }
+      attrs["pos"] = new Point(`${md["pos_x"]}${md["pos_y"]}`)
+      return attrs
     })
   }
 
@@ -99,7 +129,15 @@ class Sfen {
   }
 }
 
-if (process.argv[1] === require.main.filename) {
+export { Sfen }
+
+// console.log(process.argv)
+// console.log(process.argv[0])
+// console.log(process.argv[1])
+// console.log(require.main.filename)
+// console.log(__filename)
+
+if (process) {
   let sfen = new Sfen("position sfen +lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S2s 1 moves 7i6h S*2d")
   sfen.parse()
   // console.log(sfen.field())
@@ -107,5 +145,3 @@ if (process.argv[1] === require.main.filename) {
   // console.log(sfen.hold_pieces())
   console.log(sfen.move_infos())
 }
-
-// export { Sfen }
