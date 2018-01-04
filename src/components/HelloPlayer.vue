d71b3fca474a96ef32b273162697392d2c1d4b04
akicho8 <akicho8@gmail.com>
2018-01-04 09:55:51 +0900

    プレイヤーのコントローラー→

--- src/components/HelloPlayer.vue
+++ src/components/HelloPlayer.vue
@@ -7,8 +7,10 @@
     <input type="range" v-model.number="turn_current" :min="turn_counter_base" :max="turn_counter_max" />
   </div>
   <div>
-    <button @click="play_previous">前へ</button>
-    <button @click="play_next">次へ</button>
+    <button @click="turn_current = turn_counter_base">｜＜</button>
+    <button @click="turn_current -= 1">＜</button>
+    <button @click="turn_current += 1">＞</button>
+    <button @click="turn_current = turn_counter_max">＞｜</button>
   </div>
   <br>
 
@@ -79,13 +81,11 @@ export default {
   },
 
   watch: {
-    turn_current: function () {
-      if (this.turn_counter_base !== null) {
+    turn_current: function (new_value, old_value) {
+      if (this.sfen) {
         if (this.turn_current < this.turn_counter_base) {
           this.turn_current = this.turn_counter_base
         }
-      }
-      if (this.turn_counter_max !== null) {
         if (this.turn_counter_max < this.turn_current) {
           this.turn_current = this.turn_counter_max
         }
@@ -95,14 +95,6 @@ export default {
   },
 
   methods: {
-    play_next () {
-      this.turn_current += 1
-      this.field_update()
-    },
-    play_previous () {
-      this.turn_current -= 1
-      this.field_update()
-    },
     field_update () {
       const kifu_body = "position startpos moves 2g2f 3c3d 2f2e 2b3c 7g7f 3a4b 3i4h 5c5d 5i6h 5d5e 3g3f 8b5b 4h3g 4b5c 3g4f 5c4d 4i5h 5a6b 6h7h 6b7b 6g6f 7b8b 5h6g 7a7b 8h7g 9c9d 7h8h 9d9e 9i9h 8c8d 8h9i 7b8c 7i8h 6a7b 6f6e 7c7d 6g6f 8a7c 7g8f 5b5a 2h7h 8d8e 8f5i 3c4b 6i7i 2a3c 7f7e 7d7e 6f7e 4b7e 7h7e P*7d 7e7f G*7e 7f7h 5e5f 5g5f 7e6e 5f5e P*5f 5i2f 1c1d P*6b 4a5b 6b6a+ 5a6a 5e5d 6e6f 7h6h 6f7f P*7g 7f7e 6h5h 7e6f 5h6h 6f6e B*3b 8e8f 8g8f 3d3e 3b2c+ 6c6d 3f3e 6a2a 2c3d 9a9b 2f4h 2a9a 2e2d 6e7e 3d5f P*8d 4h7e 7d7e 6h6d 5b6c 6d6h B*6e 6h6e 7c6e 5f6e R*5i N*6f 7b7c B*5b 6c6d 6e6d 7c6d P*6e B*4h 6e6d 4h6f+ G*7c"
 
