9389dc5fa18229aa7d9d841732866f3730c52777
akicho8 <akicho8@gmail.com>
2018-01-13 14:17:47 +0900

    eslint の警告でも画面に表示

--- config/index.js
+++ config/index.js
@@ -26,7 +26,7 @@ module.exports = {
     useEslint: true,
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
-    showEslintErrorsInOverlay: false,
+    showEslintErrorsInOverlay: true,
 
     /**
      * Source Maps
