f563f03fc84a7d2c4163b8606c1ad960208dfa96
akicho8 <akicho8@gmail.com>
2018-01-15 20:57:27 +0900

    Replace to_s with toString

--- src/mediator.js
+++ src/mediator.js
@@ -124,7 +124,7 @@ class Mediator {
   }
 
   get to_sfen() {
-    return (new SfenSerializer(this)).to_s
+    return (new SfenSerializer(this)).toString
   }
 }
 
--- test/unit/specs/sfen_serializer.spec.js
+++ test/unit/specs/sfen_serializer.spec.js
@@ -8,6 +8,6 @@ describe('SfenSerializer', () => {
     mediator.current_turn = 0
     mediator.run()
     const sfen_serializer = new SfenSerializer(mediator)
-    expect(sfen_serializer.to_s).toEqual("lr5nl/2n3SB1/3gp2p1/8p/PNP1kpPP1/6p1P/2g+p5/3P1G3/L1K4RL w G2SN5Pbsp 120")
+    expect(sfen_serializer.toString).toEqual("lr5nl/2n3SB1/3gp2p1/8p/PNP1kpPP1/6p1P/2g+p5/3P1G3/L1K4RL w G2SN5Pbsp 120")
   })
 })
