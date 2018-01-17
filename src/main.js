663e9b14fda43ef984601184fadca36d14067c44
akicho8 <akicho8@gmail.com>
2018-01-17 21:41:05 +0900

    vuetify の仮導入

--- src/main.js
+++ src/main.js
@@ -2,14 +2,26 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(Vuetify)
 
-Vue.use(BootstrapVue)
+// Vue.use(Vuetify, {
+//   theme: {
+//     primary: '#0000ff',
+//     secondary: '#0000ff',
+//     accent: '#8c9eff',
+//     error: '#b71c1c'
+//   }
+// })
 
 Vue.config.productionTip = false
+
+// import BootstrapVue from 'bootstrap-vue'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
