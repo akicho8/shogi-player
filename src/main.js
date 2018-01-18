2e4f18dbe467f37040fd0ed89fcbae4f2f6eaebc
akicho8 <akicho8@gmail.com>
2018-01-18 16:11:08 +0900

    Remove vuetify

--- src/main.js
+++ src/main.js
@@ -2,19 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-
-Vue.use(Vuetify)
-
-// Vue.use(Vuetify, {
-//   theme: {
-//     primary: '#0000ff',
-//     secondary: '#0000ff',
-//     accent: '#8c9eff',
-//     error: '#b71c1c'
-//   }
-// })
 
 Vue.config.productionTip = false
 
--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,5 +1,3 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
 
-Vue.use(Vuetify)
 Vue.config.productionTip = false
