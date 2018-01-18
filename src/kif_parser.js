609e242f4dabe6a941b750364bcaffcf294e0431
akicho8 <akicho8@gmail.com>
2018-01-18 14:23:25 +0900

    座標の解釈を Piece.lookup で吸収

--- src/kif_parser.js
+++ src/kif_parser.js
@@ -5,7 +5,6 @@ import { Piece } from "./piece"
 import { Point } from "./point"
 import { Location } from "./location"
 import { SfenParser } from "./sfen_parser"
-import { Board } from "./board"
 
 class KifParser extends ParserBase {
   get field() {
@@ -28,29 +27,24 @@ class KifParser extends ParserBase {
 
   get move_infos() {
     const regexp = XRegExp(`
-^\\s*(?<number>\\d+)\\s+
-(?<to>[１-９1-9一二三四五六七八九]{2})?
-(?<same>同)?
-(?<piece>成[銀桂香]|[王玉金銀全桂圭香杏角馬飛龍竜歩と])
-(?<suffix>[左右直]?[寄引上]?)
-(?<motion>不?成|打|合|生)?
-(\\((?<origin_point>\\d{2})\\))? # KIFフォーマットの移動元用
-`, 'xm')
+        ^\\s*(?<number>\\d+)\\s+
+        (?<to>[１-９1-9一二三四五六七八九]{2})?
+        (?<same>同)?
+        (?<piece>成[銀桂香]|[王玉金銀全桂圭香杏角馬飛龍竜歩と])
+        (?<suffix>[左右直]?[寄引上]?)
+        (?<motion>不?成|打|合|生)?
+        (\\((?<origin_point>\\d{2})\\))? # KIFフォーマットの移動元用
+      `, 'xm')
 
     const list = []
     XRegExp.forEach(this.kifu_body, regexp, (md, i) => {
       const attrs = {}
       attrs["location"] = this.location_by_offset(i)
       if (md["origin_point"]) {
-        const [x, y] = md["origin_point"].split("").map((e) => parseInt(e))
-        attrs["origin_point"] = Point.fetch([Board.dimension - x, y - 1])
+        attrs["origin_point"] = Point.fetch(md["origin_point"])
       }
       if (md["to"]) {
-        let to = md["to"]
-        const table = {"１": 1, "２": 2, "３": 3, "４": 4, "５": 5, "６": 6, "７": 7, "８": 8, "９": 9, "一": 1, "二": 2, "三": 3, "四": 4, "五": 5, "六": 6, "七": 7, "八": 8, "九": 9}
-        to = to.replace(/([１-９一二三四五六七八九])/g, (e) => table[e])
-        const [x, y] = to.split("").map((e) => parseInt(e))
-        attrs["point"] = Point.fetch([Board.dimension - x, y - 1])
+        attrs["point"] = Point.fetch(md["to"])
       }
       if (md["motion"] === "成") {
         attrs["promoted_trigger"] = true
--- src/point.js
+++ src/point.js
@@ -5,15 +5,12 @@ class Point {
     return Object.freeze(new Point(v))
   }
 
-  constructor(point) {
+  constructor(value) {
     let x, y
-    if (typeof point === "string") {
-      // FIXME: sfen 専用になっているのでsfen側に移動
-      [x, y] = point.split("")
-      x = Board.dimension - Number(x)
-      y = y.charCodeAt(0) - "a".charCodeAt(0)
+    if (typeof value === "string") {
+      [x, y] = this._parse_from_string(value)
     } else {
-      [x, y] = point
+      [x, y] = value
     }
     [this._x, this._y] = [x, y]
   }
@@ -28,6 +25,21 @@ class Point {
 
   get y() {
     return this._y
+  }
+
+  _parse_from_string(s) {
+    s = s.replace(/([１-９一二三四五六七八九a-z])/g, (e) => this._replace_table[e])
+    const [x, y] = s.split("").map((e) => Number(e))
+    return [Board.dimension - x, y - 1]
+  }
+
+  get _replace_table() {
+    /* eslint-disable object-property-newline, key-spacing */
+    return {
+      "１": 1, "２": 2, "３": 3, "４": 4, "５": 5, "６": 6, "７": 7, "８": 8, "９": 9,
+      "一": 1, "二": 2, "三": 3, "四": 4, "五": 5, "六": 6, "七": 7, "八": 8, "九": 9,
+      "a":  1, "b":  2, "c":  3, "d":  4, "e":  5, "f":  6, "g":  7, "h":  8, "i":  9,
+    }
   }
 }
 
