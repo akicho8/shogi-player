46dfac291c0ed4aca34760687728c8c794b4e009
akicho8 <akicho8@gmail.com>
2018-01-20 17:59:50 +0900

    shogi_player.js を単体ビルドする

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,8 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    app: './src/main.js',
+    shogi_player: './src/components/ShogiPlayer.vue',
   },
   output: {
     path: config.build.assetsRoot,
