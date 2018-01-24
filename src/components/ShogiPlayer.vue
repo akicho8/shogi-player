dc153d63e4508ea3d9ba37822c7cd6666819e6b2
akicho8 <akicho8@gmail.com>
2018-01-24 11:23:29 +0900

    chore: add axios-debug-log

--- src/components/ShogiPlayer.vue
+++ src/components/ShogiPlayer.vue
@@ -105,6 +105,29 @@ import axios from "axios"
 import Vue from 'vue'
 Object.defineProperty(Vue.prototype, '_', {value: _})
 
+// Log content type
+// if (typeof localStorage !== "undefined") {
+//   localStorage.debug = "axios"
+// }
+require('axios-debug-log')({
+  request: function (debug, config) {
+    debug('Request with ' + config.headers['content-type'])
+  },
+  response: function (debug, response) {
+    debug(
+      'Response with ' + response.headers['content-type'],
+      'from ' + response.config.url
+    )
+  },
+  error: function (debug, error) {
+    // Read https://www.npmjs.com/package/axios#handling-errors for more info
+    debug('Boom', error)
+  }
+})
+
+const debug = require('debug')('debug')
+debug('booting %o', "ok")
+
 /* eslint-disable no-new */
 export default {
   name: 'ShogiPlayer',
