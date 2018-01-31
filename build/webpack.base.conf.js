64bc10c7a1797e7632697334387b9993d58dec1f
akicho8 <akicho8@gmail.com>
2018-01-31 11:33:55 +0900

    chore: limit 1

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -60,7 +60,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 1,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
