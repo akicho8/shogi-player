6ad0a857e11fc0a119cc81130b6402b3ca992171
akicho8 <akicho8@gmail.com>
2018-01-31 21:24:41 +0900

    fix: change assetsPath

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -61,7 +61,7 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('./img/[name].[hash:7].[ext]')
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
       {
