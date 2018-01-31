e9f56f11d6135afe39cc1d09a5dff10218eb3b90
akicho8 <akicho8@gmail.com>
2018-01-31 12:06:17 +0900

    fix: url-loader のパスを ./img に変更

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -60,8 +60,8 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 100000000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+          limit: 10000,
+          name: utils.assetsPath('./img/[name].[hash:7].[ext]')
         }
       },
       {
