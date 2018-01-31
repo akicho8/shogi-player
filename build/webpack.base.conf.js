699a336f34532a14a0f1e20be3572e7a5d935eab
akicho8 <akicho8@gmail.com>
2018-01-31 11:42:41 +0900

    fix: cssが重複している

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -23,7 +23,7 @@ module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
     app: './src/main.js',
-    shogi_player: './src/components/ShogiPlayer.vue',
+    // shogi_player: './src/components/ShogiPlayer.vue',
   },
   output: {
     path: config.build.assetsRoot,
@@ -60,7 +60,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 1,
+          limit: 100000000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
