2cf013cd009c002016fe2618ba906d3437f97727
akicho8 <akicho8@gmail.com>
2018-03-20 13:41:26 +0900

    [style] 「…」の cursor: pointer

--- src/components/ShogiPlayer.vue
+++ src/components/ShogiPlayer.vue
@@ -44,7 +44,7 @@
         <input type="number" v-model.number="turn_edit_value" @blur="turn_edit = false" ref="turn_edit_input" class="turn_edit_input">
       </template>
     </template>
-    <span class="is-pulled-right kakusi_menu" @click="isComponentModalActive = true"><b-icon icon="dots-vertical" size="is-small"></b-icon></span>
+    <span class="is-pulled-right modal_trigger_dots" @click="isComponentModalActive = true"><b-icon icon="dots-vertical" size="is-small"></b-icon></span>
     <b-modal :active.sync="isComponentModalActive" has-modal-card>
       <SettingModal :run_mode.sync="run_mode2"></SettingModal>
     </b-modal>
@@ -1055,7 +1055,7 @@ export default {
       // this.mediator.hold_pieces.set("white", v.get("black"))
     },
 
-    kakusi_menu_click() {
+    modal_trigger_dots_click() {
     },
 
     cardModal() {
