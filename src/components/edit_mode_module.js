abf6a625c5d93a86358c6eabcecd2f3eff172349
akicho8 <akicho8@gmail.com>
2018-04-06 16:06:30 +0900

    [fix] 反転させた状態で駒を持つと反転してしまう

--- src/components/edit_mode_module.js
+++ src/components/edit_mode_module.js
@@ -408,6 +408,9 @@ export default {
       const piece_inner = document.createElement("div")
       const list = _.concat(class_list, ["piece_inner"])
       piece_inner.classList.add(...list)
+      if (this.flip) {
+        piece_inner.classList.add("virtual_piece_flip") // 盤面を反転している場合は駒も反転する
+      }
       this.cursor_elem.appendChild(piece_inner)
       this.$el.appendChild(this.cursor_elem)
 
