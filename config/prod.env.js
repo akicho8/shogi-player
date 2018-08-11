0cc6e5761f87ae6a9108eedef7ec7526f8850419
akicho8 <akicho8@gmail.com>
2018-08-11 19:29:28 +0900

    [chore] STAGE=staging で設定を変更する例

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,16 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  FOO: '"a"',
 }
+
+if (process.env.STAGE === 'staging') {
+  const merge = require('webpack-merge')
+
+  module.exports = merge(module.exports, {
+    FOO: '"b"',
+  })
+}
+
+console.log("process.env.STAGE", process.env.STAGE)
+console.log("module.exports", module.exports)
