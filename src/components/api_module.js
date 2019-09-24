b7eadd018e12f3971afec455e7767c54c806f118
akicho8 <akicho8@gmail.com>
2019-09-24 13:31:31 +0900

    [chore] API追加

--- src/components/api_module.js
+++ src/components/api_module.js
@@ -2,6 +2,7 @@ import Soldier from "../soldier"
 
 export default {
   methods: {
+    // 盤を消してランダムに駒を配置する
     api_random_puton() {
       const soldier = Soldier.random()
       this.mediator.board.clear()
@@ -9,12 +10,24 @@ export default {
       return soldier
     },
 
+    // 駒を置く
     api_place_on(soldier) {
       this.mediator.board.place_on(soldier)
     },
 
+    // 盤面クリア
     api_board_clear() {
       this.mediator.board.clear()
+    },
+
+    // 指定手数の局面に設定
+    api_board_turn_set(turn) {
+      this.mediator_setup(turn)
+    },
+
+    // 反転状態の設定
+    api_flip_set(flag) {
+      this.$store.state.current_flip = flag
     },
   },
 }
