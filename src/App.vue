c067dc1607f01f018f45deee5977321f414f6196
akicho8 <akicho8@gmail.com>
2020-02-14 00:41:24 +0900

    [chore] App.vue に動作確認用の盤面を追加

--- src/App.vue
+++ src/App.vue
@@ -224,6 +224,13 @@
 
           .columns
             .column
+              article.message.is-info
+                .message-body
+                  | SFENで72となっていても71手目の局面を表示している(後手番)
+              ShogiPlayer(:kifu_body="'position sfen lr4knl/3g2gs1/4ppP2/p4bNpp/2pSsN3/PPPP1P2P/2N1P1G2/2G6/L1K4RL w BPs3p 72'" :start_turn="-1" :theme="'simple'" :run_mode="'play_mode'")
+
+          .columns
+            .column
               article.message.is-success.has-text-left
                 .message-header
                   p 操作モードについて
