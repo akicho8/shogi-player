0a673e4c71d070d505de4fe21a9cbf0aa5f7281a
akicho8 <akicho8@gmail.com>
2020-06-18 17:14:32 +0900

    [chore] 「詰将棋」は一番タイプするので一番上

--- src/preset_info.js
+++ src/preset_info.js
@@ -5,11 +5,11 @@ export default class PresetInfo extends MemoryRecord {
   static get define() {
     return [
       /* eslint-disable */
-      { key: "全部駒箱",                sfen: "position sfen 9/9/9/9/9/9/9/9/9 b - 1",                                                       first_location_key: "black", piece_box: [["B", 2], ["R", 2], ["L", 4], ["N", 4], ["S", 4], ["G", 4], ["P", 18], ["K", 2]]},
       { key: "詰将棋",                  sfen: "position sfen 4k4/9/9/9/9/9/9/9/9 b 2r2b4g4s4n4l18p 1",                                       first_location_key: "black", piece_box: [["K", 1]]},
       { key: "詰将棋 - 美濃",           sfen: "position sfen ln1g5/1ks6/1ppp5/p8/9/9/9/9/9 b BGSNL2rb2g2s2n2l14p 1",                         first_location_key: "black", piece_box: [["K", 1]]},
       { key: "詰将棋 - 矢倉",           sfen: "position sfen 7nl/6gk1/5gspp/5pp2/9/9/9/9/9 b BGSNL2rbg2s2n2l14p 1",                          first_location_key: "black", piece_box: [["K", 1]]},
       { key: "戦型 右四間 vs 四間飛車", sfen: "position sfen ln1g1g1nl/1ks2r3/1pppp1bpp/p3spp2/9/P1P1SP1PP/1P1PP1P2/1BK1GR3/LNSG3NL b - 25", first_location_key: "black", piece_box: []},
+      { key: "全部駒箱",                sfen: "position sfen 9/9/9/9/9/9/9/9/9 b - 1",                                                       first_location_key: "black", piece_box: [["B", 2], ["R", 2], ["L", 4], ["N", 4], ["S", 4], ["G", 4], ["P", 18], ["K", 2]]},
       { key: "平手",                    sfen: "position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1",               first_location_key: "black", piece_box: []},
       { key: "香落ち",                  sfen: "position sfen lnsgkgsn1/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1",               first_location_key: "white", piece_box: [["L", 1]]},
       { key: "右香落ち",                sfen: "position sfen 1nsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1",               first_location_key: "white", piece_box: [["L", 1]]},
