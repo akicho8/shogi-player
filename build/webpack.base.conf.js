a83a4014e8d8e0a01aa2e5c9f16b0eaa8c3dfc09
akicho8 <akicho8@gmail.com>
2020-11-10 16:36:17 +0900

    [refactor] eslint 関連パッケージの全削除

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -8,17 +8,6 @@ function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-const createLintingRule = () => ({
-  test: /\.(js|vue)$/,
-  loader: 'eslint-loader',
-  enforce: 'pre',
-  include: [resolve('src'), resolve('test')],
-  options: {
-    formatter: require('eslint-friendly-formatter'),
-    emitWarning: !config.dev.showEslintErrorsInOverlay
-  }
-})
-
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
@@ -41,7 +30,6 @@ module.exports = {
   },
   module: {
     rules: [
-      ...(config.dev.useEslint ? [createLintingRule()] : []),
       {
         test: /\.(txt|md|kif|ki2|csa|sfen)$/,
         use: 'raw-loader',
