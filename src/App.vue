385c1949d60142700960cf4b972e7ad1846932d9
akicho8 <akicho8@gmail.com>
2020-11-13 20:49:16 +0900

    [chore] 盤の駒以外は background-position: center とする

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   template(v-if="true")
     .virtual_screen
       shogi-player(
-        :run_mode="'view_mode'"
+        :run_mode="'play_mode'"
         :debug_mode="false"
         :start_turn="0"
         :kifu_body="'position sfen 4R1gnk/6+Bsl/5+P1pp/9/9/9/9/9/9 b rb3g3s3n2l15pR3BG18SN 1 moves 3b2a 3a2a 5a2a+ 1a2a G*3b 2a1a 3b2b 1a2b N*3d 2b1a S*2b'"
