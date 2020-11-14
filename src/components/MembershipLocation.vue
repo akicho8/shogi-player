5e83f87dcd2c322e331566438b93221baeedd9c6
akicho8 <akicho8@gmail.com>
2020-11-14 18:06:05 +0900

    [refactor] MembershipLocationMark 追加

--- src/components/MembershipLocation.vue
+++ src/components/MembershipLocation.vue
@@ -1,21 +1,23 @@
 <template lang="pug">
 .MembershipLocation.is-flex
-  .location_mark(v-html="location.name")
+  MembershipLocationMark(:base="base" :location="location")
   .player_name.has-text-weight-bold(v-if="player_name")
     | {{player_name}}
 </template>
 
 <script>
 import { support_child } from "./support_child.js"
+import MembershipLocationMark from "./MembershipLocationMark.vue"
 
 export default {
   name: "MembershipLocation",
   mixins: [support_child],
-
   props: {
     location: { required: true },
   },
-
+  components: {
+    MembershipLocationMark,
+  },
   computed: {
     player_name() {
       if (this.player_info) {
@@ -44,16 +46,6 @@ export default {
       .MembershipLocation
         margin-left: auto  // ▲だけ右寄せ (右寄せにしたければ左側のマージンをautoにする)
         font-size: 1rem
-        .location_mark
-          color: $sp_simple_grid_color
-
-      //////////////////////////////////////////////////////////////////////////////// 手番のとき
-      &.turn_active
-        .MembershipLocation
-          .location_mark
-            color: $sp_simple_turn_active_color
-            -webkit-text-stroke: 1px $sp_simple_turn_active_color
-            text-shadow: 0 0 0.5rem $sp_simple_turn_active_color
 
   &.vertical
     // 反転してない状態で上の最初から反転している「△後手」を先手目線にするため反転する
---
+++ src/components/MembershipLocationMark.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+.MembershipLocationMark(v-text="location.name")
+</template>
+
+<script>
+import { support_child } from "./support_child.js"
+
+export default {
+  name: "MembershipLocationMark",
+  mixins: [support_child],
+  props: {
+    location: { required: true },
+  },
+}
+</script>
+
+<style lang="sass">
+@import "./support.sass"
+.shogi-player
+  &.vertical
+    .MembershipLocationMark
+      color: $sp_simple_grid_color
+
+    //////////////////////////////////////////////////////////////////////////////// 手番のとき
+    .turn_active
+      .MembershipLocationMark
+        color: $sp_simple_turn_active_color
+        -webkit-text-stroke: 1px $sp_simple_turn_active_color
+        text-shadow: 0 0 0.5rem $sp_simple_turn_active_color
+</style>
