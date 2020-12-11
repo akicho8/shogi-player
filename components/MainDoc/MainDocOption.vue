4b954a8d0f5ab0352e931930065607f24c96acbb
akicho8 <akicho8@gmail.com>
2020-12-11 20:09:01 +0900

    [feat] MainDocOption.vue

--- components/MainDoc/MainDocOption.vue
+++ components/MainDoc/MainDocOption.vue
@@ -3,8 +3,8 @@
   b-sidebar.MainDocOption-Sidebar(fullheight right v-model="sidebar_p" position="fixed" open)
     .mx-4.my-4
       .is-flex.is-justify-content-start.is-align-items-center
-        b-button(@click="sidebar_toggle" icon-left="menu")
-        .mx-3.has-text-weight-bold オプション
+        b-button(@click="sidebar_toggle" icon-left="close")
+        //- .mx-3.has-text-weight-bold オプション
 
       .my_controls
         .box
@@ -13,8 +13,42 @@
               b-radio-button(size="is-small" v-model="run_mode" :native-value="e.key") {{e.name}}
 
         .box
-          .title.is-5 テクスチャ
+          .title.is-5 棋譜
+          b-field(custom-class="is-small" label="kifu_body")
+            b-input(size="is-small" v-model="kifu_body" type="textarea")
+          b-field(custom-class="is-small" label="start_turn")
+            //- b-slider(v-model="start_turn" :min="-1" :max="256" step="1")
+            b-input(size="is-small" v-model="start_turn" type="number")
 
+        .box
+          .title.is-5 対局者情報
+          b-field(custom-class="is-small" label="player_info.black.name")
+            b-input(size="is-small" v-model.trim="player_info.black.name" type="text")
+          b-field(custom-class="is-small" label="player_info.white.name")
+            b-input(size="is-small" v-model.trim="player_info.white.name" type="text")
+
+        .box
+          .title.is-5 レイアウト
+          b-field(custom-class="is-small" label="配置")
+            b-radio-button(size="is-small" v-model="sp_layout" native-value="is_vertical") 上下
+            b-radio-button(size="is-small" v-model="sp_layout" native-value="is_horizontal") 左右
+          MainDocSwitch(v-model="setting_button_show" label="setting_button_show")
+          MainDocSwitch(v-model="summary_show" label="summary_show")
+          MainDocSwitch(v-model="slider_show" label="slider_show")
+          MainDocSwitch(v-model="controller_show" label="controller_show")
+          MainDocSwitch(v-model="sfen_show" label="sfen_show")
+          MainDocSwitch(v-model="flip" label="flip")
+          MainDocSwitch(v-model="flip_if_white" label="flip_if_white")
+          MainDocSwitch(v-model="hidden_if_piece_stand_blank" label="hidden_if_piece_stand_blank")
+
+        .box
+          .title.is-5 効果音
+          MainDocSwitch(v-model="sound_effect" label="sound_effect")
+          b-field(custom-class="is-small" label="volume")
+            b-slider(v-model="volume" :min="0" :max="1.0" step="0.01")
+
+        .box
+          .title.is-5 テクスチャ
           b-field(custom-class="is-small" label="sp_bg_variant")
             b-select(size="is-small" v-model="sp_bg_variant")
               template(v-for="e in BgVariantInfo.values")
@@ -26,59 +60,53 @@
                 option(:value="e.key") {{e.name}}
 
         .box
-          .title.is-5 効果音
-
-          MainDocSwitch(v-model="sound_effect" label="sound_effect")
-
-          b-field(custom-class="is-small" label="volume")
-            b-slider(v-model="volume" :min="0" :max="1.0" step="0.01")
-
-        .box
-          b-field(custom-class="is-small" label="start_turn")
-            b-input(v-model="start_turn" type="number")
+          .title.is-5 その他
           b-field(custom-class="is-small" label="final_label")
-            b-input(v-model.trim="final_label" type="text")
+            b-input(size="is-small" v-model.trim="final_label" type="text")
+          MainDocSwitch(v-model="overlay_navi" label="overlay_navi")
+          MainDocSwitch(v-model="key_event_capture" label="key_event_capture")
+          MainDocSwitch(v-model="debug_mode_p" label="debug_mode_p")
+          MainDocSwitch(v-model="operation_disable" label="operation_disable")
+          b-field(custom-class="is-small" label="human_side_key")
+            template(v-for="e in HumanSideInfo.values")
+              b-radio-button(size="is-small" v-model="human_side_key" :native-value="e.key") {{e.name}}
 
         .box
-          b-field(custom-class="is-small" label="player_info.black.name")
-            b-input(v-model.trim="player_info.black.name" type="text")
-          b-field(custom-class="is-small" label="player_info.white.name")
-            b-input(v-model.trim="player_info.white.name" type="text")
+          .title.is-5 操作モードのイベント受信内容
 
-        b-field(custom-class="is-small" label="slider_show")
-          b-radio-button(size="is-small" v-model="slider_show" :native-value="false") OFF
-          b-radio-button(size="is-small" v-model="slider_show" :native-value="true")  ON
+          MainDocSwitch(v-model="trigger_toast_p" label="イベント確認")
 
-        MainDocSwitch(v-model="controller_show" label="controller_show")
-        MainDocSwitch(v-model="sfen_show" label="sfen_show")
-        MainDocSwitch(v-model="overlay_navi" label="overlay_navi")
-        MainDocSwitch(v-model="key_event_capture" label="key_event_capture")
-        MainDocSwitch(v-model="flip" label="flip")
-        MainDocSwitch(v-model="flip_if_white" label="flip_if_white")
-        MainDocSwitch(v-model="debug_mode_p" label="debug_mode_p")
-        MainDocSwitch(v-model="hidden_if_piece_stand_blank" label="hidden_if_piece_stand_blank")
-        MainDocSwitch(v-model="setting_button_show" label="setting_button_show")
-        MainDocSwitch(v-model="summary_show" label="summary_show")
-        MainDocSwitch(v-model="operation_disable" label="operation_disable")
+          b-field(custom-class="is-small" label="@update:play_mode_advanced_full_moves_sfen: 操作モードで指した直後の局面を発行(movesあり)")
+            b-input(size="is-small" :value="JSON.stringify(play_mode_advanced_full_moves_sfen)" readonly type="textarea" rows="1")
 
-        b-field(custom-class="is-small" label="配置")
-          b-radio-button(size="is-small" v-model="sp_layout" native-value="is_vertical") 上下
-          b-radio-button(size="is-small" v-model="sp_layout" native-value="is_horizontal") 左右
+          b-field(custom-class="is-small" label="@update:play_mode_advanced_snapshot_sfen: 操作モードで指した直後の局面を発行(movesなし)")
+            b-input(size="is-small" :value="JSON.stringify(play_mode_advanced_snapshot_sfen)" readonly type="textarea")
 
-        b-field(custom-class="is-small" label="human_side_key")
-          template(v-for="e in HumanSideInfo.values")
-            b-radio-button(v-model="human_side_key" :native-value="e.key") {{e.key}}
+          b-field(custom-class="is-small" label="@update:play_mode_advanced_last_move: 操作モードで指した手(sfenのmovesの最後の1つ)")
+            b-input(size="is-small" :value="JSON.stringify(play_mode_advanced_last_move)" readonly type="textarea")
 
-        b-field(custom-class="is-small" label="kifu_body (KIF or USI(sfen)形式")
-          b-input(v-model="kifu_body" type="textarea")
+          b-field(custom-class="is-small" label="@update:play_mode_advanced_moves: 操作モードで指した手を含むmoves配列")
+            b-input(size="is-small" :value="JSON.stringify(play_mode_advanced_moves)" readonly type="textarea")
+
+          b-field(custom-class="is-small" label="@update:mediator_snapshot_sfen: 操作モード(または再生モード)で盤面が変化したとき(常に更新)")
+            b-input(size="is-small" :value="JSON.stringify(mediator_snapshot_sfen)" readonly type="textarea")
+
+          b-field(custom-class="is-small" label="@update:edit_mode_snapshot_sfen: 編集モードで盤面が変化したとき")
+            b-input(size="is-small" :value="JSON.stringify(edit_mode_snapshot_sfen)" readonly type="textarea")
+
+          b-field(custom-class="is-small" label="@update:turn_offset: 手数が変化したとき")
+            b-input(size="is-small" :value="JSON.stringify(turn_offset)" readonly type="text")
+
+          b-field(custom-class="is-small" label="@update:moves_take_turn_offset: 操作モードでmovesをturn_offsetでtakeしたもの")
+            b-input(size="is-small" :value="JSON.stringify(moves_take_turn_offset)" readonly type="text")
 
   MainDocMainNavbar
     template(slot="brand")
       MainDocNavbarItemHome
       b-navbar-item(tag="div").has-text-weight-bold オプション
     template(slot="end")
-      b-navbar-item(@click="sidebar_toggle")
-        b-icon(icon="menu")
+      b-navbar-item.has-text-weight-bold(@click="sidebar_toggle")
+        | テスト
 
   //- この form は prevent が効いているか確認するためのもの
   .section
@@ -91,6 +119,7 @@
             :start_turn.sync="start_turn"
             :sp_bg_variant.sync="sp_bg_variant"
             :sp_pi_variant.sync="sp_pi_variant"
+            :sp_layout="sp_layout"
             :debug_mode_p.sync="debug_mode_p"
             :hidden_if_piece_stand_blank="hidden_if_piece_stand_blank"
             :setting_button_show="setting_button_show"
@@ -98,7 +127,6 @@
             :operation_disable="operation_disable"
             :flip.sync="flip"
             :flip_if_white="flip_if_white"
-            :sp_layout="sp_layout"
             :final_label="final_label"
             :player_info="player_info"
             :key_event_capture="key_event_capture"
@@ -157,36 +185,6 @@
                 |   :player_info='{{JSON.stringify(player_info)}}'
                 |   :kifu_body="'{{kifu_body}}'"
 
-            .title.is-4 操作モードのイベント受信内容
-
-            b-field(custom-class="is-small" label="イベント確認(buefy.toast)")
-              b-switch(v-model="trigger_toast_p")
-
-            b-field(custom-class="is-small" label="@update:play_mode_advanced_full_moves_sfen: 操作モードで指した直後の局面を発行(movesあり)")
-              b-input(:value="JSON.stringify(play_mode_advanced_full_moves_sfen)" readonly type="textarea" rows="1")
-
-            b-field(custom-class="is-small" label="@update:play_mode_advanced_snapshot_sfen: 操作モードで指した直後の局面を発行(movesなし)")
-              b-input(:value="JSON.stringify(play_mode_advanced_snapshot_sfen)" readonly type="text")
-
-            b-field(custom-class="is-small" label="@update:play_mode_advanced_last_move: 操作モードで指した手(sfenのmovesの最後の1つ)")
-              b-input(:value="JSON.stringify(play_mode_advanced_last_move)" readonly type="text")
-
-            b-field(custom-class="is-small" label="@update:play_mode_advanced_moves: 操作モードで指した手を含むmoves配列")
-              b-input(:value="JSON.stringify(play_mode_advanced_moves)" readonly type="text")
-
-            b-field(custom-class="is-small" label="@update:mediator_snapshot_sfen: 操作モード(または再生モード)で盤面が変化したとき(常に更新)")
-              b-input(:value="JSON.stringify(mediator_snapshot_sfen)" readonly type="text")
-
-            b-field(custom-class="is-small" label="@update:edit_mode_snapshot_sfen: 編集モードで盤面が変化したとき")
-              b-input(:value="JSON.stringify(edit_mode_snapshot_sfen)" readonly type="text")
-
-            b-field(custom-class="is-small" label="@update:turn_offset: 手数が変化したとき")
-              b-input(:value="JSON.stringify(turn_offset)" readonly type="text")
-
-            b-field(custom-class="is-small" label="@update:moves_take_turn_offset: 操作モードでmovesをturn_offsetでtakeしたもの")
-              b-input(:value="JSON.stringify(moves_take_turn_offset)" readonly type="text")
-            br
-
       MainDocMd(:body="options_md")
       MainDocEditDesc2
 </template>
@@ -241,7 +239,7 @@ export default {
       },
 
       final_label: null,
-      kifu_body: "position startpos moves 2g2f 3c3d 2f2e 2b3c 7g7f 3a2b 3i4h 4a3b 3g3f 7a6b 4h3g 5a4a 3g4f 6a5b 3f3e 3d3e 4f3e 5c5d P*3d 3c4b 2e2d 2c2d 3e2d 4b6d P*2c 2b2c 2d2c+ 6d2h+ 2c3b 4a3b 8h1a+ S*2b 1a2a 3b2a S*3c B*1e L*3g P*3b N*2d",
+      kifu_body: "position startpos moves 2g2f 3c3d 2f2e 2b3c",
 
       trigger_toast_p: false,
 
@@ -285,21 +283,17 @@ html
   +tablet
     --main_doc_option_sidebar_width: 50%
   +desktop
-    --main_doc_option_sidebar_width: 30%
+    --main_doc_option_sidebar_width: 40%
 
 .MainDocOption-Sidebar
   .sidebar-content
     width: var(--main_doc_option_sidebar_width)
-    // width: unset
-    // a
-    //   white-space: nowrap
   .box
     margin-top: 1rem
     margin-bottom: 0
   .title
     margin-top: 0.4rem
     margin-bottom: 1rem
-
   .b-slider
     margin-top: 0.5rem
 
--- components/models/human_side_info.js
+++ components/models/human_side_info.js
@@ -4,10 +4,10 @@ import Location from "./location"
 export default class HumanSideInfo extends MemoryRecord {
   static get define() {
     return [
+      { key: "none",   name: "none", location_keys: [],                 },
       { key: "both",   name: "☗☖", location_keys: ["black", "white"], },
       { key: "black",  name: "☗",   location_keys: ["black"],          },
       { key: "white",  name: "☖",   location_keys: ["white"],          },
-      { key: "none",   name: "なし", location_keys: [],                 },
     ]
   }
 
