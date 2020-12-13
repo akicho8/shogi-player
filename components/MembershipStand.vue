7ae1c0a855c28ae822757f0042171940e5c4d6be
akicho8 <akicho8@gmail.com>
2020-12-13 15:37:49 +0900

    [fix] 横配置 + edit_mode で駒が駒台に置けない

--- components/MembershipStand.vue
+++ components/MembershipStand.vue
@@ -90,8 +90,10 @@ export default {
 
     component_class() {
       const list = []
-      if (this.holding_p) {
-        list.push("hoverable_p")
+      if (this.base.edit_p) {
+        if (this.holding_p) {
+          list.push("hoverable_p")
+        }
       }
       return list
     },
@@ -107,13 +109,19 @@ export default {
 @import "./support.sass"
 .ShogiPlayerGround
   +defvar(sp_stand_hover_border_color, rgba(0, 0, 0, 0.2)) // 駒を持って駒箱の上にいるときのボーダー色
-  +defvar(sp_stand_hover_border_stroke, 2px) // 駒を持って駒箱の上にいるときのボーダーの太さ
+  +defvar(sp_stand_hover_border_stroke, 2px)               // 駒を持って駒箱の上にいるときのボーダーの太さ
+  +defvar(sp_stand_horizontal_hoverable_min_height, 3)     // edit_mode + 縦配置 + 駒台に置ける のときの駒台の最低限の高さ(駒N個分)
 
   .MembershipStand
     display: flex
-    justify-content: center
+    justify-content: center // 駒台が広がったときに中央に寄るのを防ぎたいときは flex-start にする
     align-items: center
 
+    // 最低限の大きさを確保するには？
+    //
+    //   min-width:  var(--sp_stand_piece_w)
+    //   min-height: var(--sp_stand_piece_h)
+    //
     // もし駒台を最初から見せる場合は？
     //
     //   width: 100%
@@ -130,26 +138,31 @@ export default {
   //////////////////////////////////////////////////////////////////////////////// 駒を持って駒箱の上にいるとき
   .MembershipStand
     &.hoverable_p
-      min-width: var(--sp_stand_piece_w)
-      min-height: var(--sp_stand_piece_h)
       &:hover
         .MembershipStandTexture
           border: var(--sp_stand_hover_border_stroke) dashed var(--sp_stand_hover_border_color)
   // 駒がなくても駒が置けるようにする
+  &.is_horizontal
+    .MembershipStand
+      &.hoverable_p
+        min-height: calc(var(--sp_stand_piece_h) * var(--sp_stand_horizontal_hoverable_min_height)) // 最低限縦に駒3つ分を確保
+        justify-content: flex-start                   // そうすると既存の駒が中央によってしまうので上寄せ
+        min-width:  var(--sp_stand_piece_w)           // 横を最低限確保
   &.is_vertical
     .MembershipStand
       &.hoverable_p
-        width: 100%                     // 駒がなくても駒台に置けるようにするため横幅最大化
-        justify-content: flex-start     // そうすると既存の駒が中央によってしまうので左寄せ
-        min-height: var(--sp_stand_piece_h)
-  ////////////////////////////////////////////////////////////////////////////////
+        width: 100%                         // 駒がなくても駒台に置けるようにするため横幅最大化
+        justify-content: flex-start         // そうすると既存の駒が中央によってしまうので左寄せ
+        min-height: var(--sp_stand_piece_h) // 縦を最低限確保
 
+  ////////////////////////////////////////////////////////////////////////////////
   &.is_layer_on
     .MembershipStand
       border: 1px dashed change_color($primary, $alpha: 0.5)
       .PieceObject
         border: 1px dashed change_color($primary, $alpha: 0.5)
 
+  ////////////////////////////////////////////////////////////////////////////////
   &.is_horizontal
     .is_white
       .MembershipStand
