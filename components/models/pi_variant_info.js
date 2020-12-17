88e24726900f196bc9cf2a54967021dd1257a6d1
akicho8 <akicho8@gmail.com>
2020-12-17 19:54:24 +0900

    [feat] remove a1bw

--- components/models/pi_variant_info.js
+++ components/models/pi_variant_info.js
@@ -5,7 +5,6 @@ export default class PiVariantInfo extends MemoryRecord {
     return [
       { key: "is_pi_variant_none", name: "none",                        },
       { key: "is_pi_variant_a1by", name: "☗ゴシック(1文字・黄)",       },
-      // { key: "is_pi_variant_a1bw", name: "☗ゴシック(1文字・白)",       },
       { key: "is_pi_variant_a2by", name: "☗ゴシック(2文字・黄)",       },
       { key: "is_pi_variant_b",    name: "紙面風",                      },
       { key: "is_pi_variant_c",    name: "☗図案駒(画像)",              },
