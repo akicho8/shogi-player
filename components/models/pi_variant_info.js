c7de716199890159c6b2005e7d8a53600d75c0fc
akicho8 <akicho8@gmail.com>
2020-12-24 11:26:50 +0900

    [feat] 解像度が低い駒Dは削除

--- components/models/pi_variant_info.js
+++ components/models/pi_variant_info.js
@@ -4,14 +4,13 @@ export default class PiVariantInfo extends MemoryRecord {
   static get define() {
     return [
       { key: "is_pi_variant_none", name: "none",                        },
-      { key: "is_pi_variant_a1by", name: "☗ゴシック(1文字・黄)",       },
-      { key: "is_pi_variant_a2by", name: "☗ゴシック(2文字・黄)",       },
+      { key: "is_pi_variant_a1by", name: "ゴシック(1文字)",             },
+      { key: "is_pi_variant_a2by", name: "ゴシック(2文字)",             },
       { key: "is_pi_variant_b",    name: "紙面風",                      },
-      { key: "is_pi_variant_c",    name: "☗図案駒(画像)",              },
-      { key: "is_pi_variant_d",    name: "☗毛筆体(画像・解像度低)",    },
-      { key: "is_pi_variant_e",    name: "orangain/shogi-piece-images", },
-      { key: "is_pi_variant_f",    name: "プチッチ(画像・暗い)",        },
-      // { key: "is_pi_variant_g",    name: "g",                        },
+      { key: "is_pi_variant_c",    name: "図案駒(画像)",                },
+      { key: "is_pi_variant_d",    name: "orangain/shogi-piece-images", },
+      { key: "is_pi_variant_e",    name: "プチッチ(画像・暗め)",        },
+      { key: "is_pi_variant_f",    name: "テスト中",                    },
     ]
   }
 }
