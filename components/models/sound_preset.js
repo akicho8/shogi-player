0e775113d16ba4c20683c1e3071f91f6e815501c
akicho8 <akicho8@gmail.com>
2021-05-28 11:49:32 +0900

    [refactor] 音ソースは require で直接書けばよかったんかい

--- components/models/sound_preset.js
+++ components/models/sound_preset.js
@@ -1,13 +1,10 @@
-import piece_put from "../../assets/Universal_Sound_FX/TABLE_TENNIS_Racket_Ball_Hit_07_Hard_mono.wav"
-import flip_sound from "../../assets/Universal_Sound_FX/RELOAD_Rechamber_Leaver_Action_stereo.wav"
-
 import MemoryRecord from 'js-memory-record'
 
 export class SoundPreset extends MemoryRecord {
   static get define() {
     return [
-      { key: "piece_put",  source: piece_put,  volume: 0.5, },
-      { key: "flip_sound", source: flip_sound, volume: 1.0, },
+      { key: "piece_put",  source: require("../../assets/Universal_Sound_FX/TABLE_TENNIS_Racket_Ball_Hit_07_Hard_mono.wav"), volume: 0.5, },
+      { key: "flip_sound", source: require("../../assets/Universal_Sound_FX/RELOAD_Rechamber_Leaver_Action_stereo.wav"),     volume: 1.0, },
     ]
   }
 }
