c5343bf07b65f3d8573bef05b63a9a99ff47361f
akicho8 <akicho8@gmail.com>
2021-06-08 19:31:36 +0900

    [style] 移動元の色再調整

--- components/PieceTap.vue
+++ components/PieceTap.vue
@@ -60,10 +60,11 @@ export default {
   +defvar(sp_piece_origin_color, hsla(0, 0%, 0%, 0.1))     // 最後に動かした駒の元の位置の背景色
   +defvar(sp_piece_selectable_color, hsla(0, 0%, 0%, 0.1))  // 持ち上げれる駒の背景色
 
-  +defvar(sp_lifted_origin_bg_color_desktop, #{change_color($primary, $saturation: 0%, $alpha: 0.1)})  // 持ち上げた駒の背景色(desktop)
+  // +defvar(sp_lifted_origin_bg_color_desktop, #{change_color($primary, $saturation: 0%, $alpha: 0.1)})  // 持ち上げた駒の背景色(desktop)
+  +defvar(sp_lifted_origin_bg_color_desktop, hsla(0, 0%, 0%, 0.1))  // 持ち上げた駒の背景色(desktop)
   +defvar(sp_lifted_origin_opacity_desktop, 0.0)                    // 持ち上げた駒の元のセルの非透明度(desktop)
 
-  +defvar(sp_lifted_origin_bg_color_touch, #{change_color($primary, $saturation: 0%, $alpha: 0.1)})      // 持ち上げた駒の背景色(touch)
+  +defvar(sp_lifted_origin_bg_color_touch, #{change_color($primary, $alpha: 0.3)})      // 持ち上げた駒の背景色(touch)
   +defvar(sp_lifted_origin_opacity_touch, 1.0)              // 持ち上げた駒の元のセルの非透明度(touch)
 
   //////////////////////////////////////////////////////////////////////////////// >= tablet
