d5819c39aa59391c3c03bf3a5c820c581a679e5d
akicho8 <akicho8@gmail.com>
2022-02-02 20:43:32 +0900

    [refactor] vue_sound.js 削除

--- plugins/mixin_mod.client.js
+++ plugins/mixin_mod.client.js
@@ -1,11 +1,9 @@
 import Vue from "vue"
 
 import vue_application from "./application.client.js"
-import vue_sound from "./vue_sound.js"
 
 Vue.mixin({
   mixins: [
     vue_application,
-    vue_sound,
   ],
 })
