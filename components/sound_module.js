f631503d1032e9bb4f7677059be90e0da696b8bd
akicho8 <akicho8@gmail.com>
2022-02-03 16:16:19 +0900

    [feat] アプリ内で Howler インスタンスが重複するため直接の import を避ける

--- components/sound_module.js
+++ components/sound_module.js
@@ -1,11 +1,21 @@
 import { SoundPresetInfo } from "./models/sound_preset_info.js"
-import { Howl, Howler } from "howler"
+
+// import { Howl, Howler } from "howler"
+//
+// ・ここの Howl で再生するとここの Howler._howls.length が増えていく
+// ・shogi-player を使っているアプリ側の Howler._howls.length は 0 のまま
+// ・だからアプリ側で Howler.unload() しても、shogi-player 側の Howler._howls は残ったままになる (実証済み)
+// ・ここの Howler とアプリ側の Howler を同じものにしたいが方法がわからない
+// ・なので Howl だけ sp_sound_howl で渡してもらうことにする
+// ・↑やめ
+// ・window.Howl が定義されていれば渡す ← こっち
 
 export const sound_module = {
   props: {
     sp_sound_enabled:      { type: Boolean, default: false, }, // サウンド
     sp_sound_body_changed: { type: Boolean, default: true,  }, // sp_body をあとから変更して内容が変わったときに音を出すか？
     sp_sound_volume:       { type: Number,  default: 0.5,   }, // ボリューム
+    // sp_sound_howl:         { type: Object,  default: null,  }, // Howl を渡してもらう
   },
   methods: {
     sound_play(key, options = {}) {
@@ -20,14 +30,30 @@ export const sound_module = {
           ...options,
         }
         // https://github.com/goldfire/howler.js#documentation
-        return new Howl(params)
+
+        if (typeof Howl !== 'undefined') {
+          return new Howl(params)
+        }
+
+        // if (this.sp_sound_howl) {
+        //   return new this.sp_sound_howl(params)
+        // }
+        // return new Howl(params)
       }
     },
 
     // スマホで音が出なくなったのを直す
     // これが必要だということは、ここの Howler と他で import した Howler は別ものなのか……？？？
     sp_sound_resume_all() {
-      Howler.unload()
+      if (typeof Howler !== 'undefined') {
+        return Howler.unload()
+      }
+    },
+
+    sp_Howler() {
+      if (typeof Howler !== 'undefined') {
+        return Howler
+      }
     },
   },
 }
--- plugins/mixin_mod.client.js
+++ plugins/mixin_mod.client.js
@@ -2,6 +2,9 @@ import Vue from "vue"
 
 import vue_application from "./application.client.js"
 
+// window.Howler と window.Howl を定義する
+import { Howl, Howler } from "howler"
+
 Vue.mixin({
   mixins: [
     vue_application,
