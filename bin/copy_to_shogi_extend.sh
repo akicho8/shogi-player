d67c43ab72ef490182c544b8300337ebd4312b4b
akicho8 <akicho8@gmail.com>
2022-09-23 17:37:59 +0900

    [chore] copy_to_shogi_extend.sh 改善

--- bin/copy_to_shogi_extend.sh
+++ bin/copy_to_shogi_extend.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avzu --delete --exclude=".git" --exclude="node_modules" --exclude=".nuxt" . ~/src/shogi-extend/nuxt_side/node_modules/shogi-player/
+rsync -avzu --delete --exclude=".git" --exclude="node_modules" --exclude=".nuxt" ~/src/shogi-player ~/src/shogi-extend/nuxt_side/node_modules/
