69c2884fc617fb6a12f48cf1812aa13c90b12306
akicho8 <akicho8@gmail.com>
2022-09-26 19:35:39 +0900

    [chore][deploy] nuxt generate --dotenv .env.production

--- docs/_nuxt/abfd3cb.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10,9,11,12,45,46,47,49,50,51,52,58,63,65,66],{2:function(n,c,e){"use strict";e.d(c,"a",(function(){return o}));var o={inject:["TheSp"]}}}]);
