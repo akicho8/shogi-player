b2acf10be126621c9c7626f21de26b02ad0d14e2
akicho8 <akicho8@gmail.com>
2022-10-02 21:20:19 +0900

    [fix] [test] テストと実装の不整合

--- test/move_info.spec.js
+++ test/move_info.spec.js
@@ -35,7 +35,7 @@ describe("MoveInfo", () => {
   })
 
   it("to_yomiage", () => {
-    expect(new MoveInfo({type: "move",       from: soldier_77P0, to: soldier_76P0}).to_yomiage).toEqual("なな 6 ふーっ！")
+    expect(new MoveInfo({type: "move",       from: soldier_77P0, to: soldier_76P0}).to_yomiage).toEqual("なな 6 ふ！")
     expect(new MoveInfo({type: "move",       from: soldier_77P1, to: soldier_76P1}).to_yomiage).toEqual("なな 6 ときんっ！")
     expect(new MoveInfo({type: "promotable", from: soldier_77P0, to: soldier_76P1}).to_yomiage).toEqual("なな 6 ふ、なりっ！")
     expect(new MoveInfo({type: "promotable", from: soldier_77P0, to: soldier_76P0}).to_yomiage).toEqual("なな 6 ふ、ならずっ！")
