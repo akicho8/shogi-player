fb47f0ba7aecdbb38b3a0a45485aa6a988bf0fa9
akicho8 <akicho8@gmail.com>
2022-10-16 15:58:23 +0900

    [refactor] mobile_p は汎用的すぎてグローバルなメソッドと干渉する

--- components/app_focus.js
+++ components/app_focus.js
@@ -10,7 +10,7 @@ export const app_focus = {
   },
   beforeMount() {
     if (this.debug_p) {
-      console.log(`mobile_p: ${this.mobile_p}`)
+      console.log(`sp_mobile_p: ${this.sp_mobile_p}`)
       console.log(`devise_info.key: ${this.devise_info.key}`)
       console.log(`focus_disable_p: ${this.focus_disable_p}`)
     }
@@ -64,9 +64,9 @@ export const app_focus = {
     },
     // モバイル端末または実際タッチできる端末であればキーボードはないと見なしてスライダーにフォーカスしない
     focus_disable_p() {
-      return this.mobile_p || this.devise_info.key === "is_device_touch"
+      return this.sp_mobile_p || this.devise_info.key === "is_device_touch"
     },
-    mobile_p() {
+    sp_mobile_p() {  // mobile_p にするとグローバルで定義したものと干渉する
       return isMobile(window.navigator).any
     },
   },
