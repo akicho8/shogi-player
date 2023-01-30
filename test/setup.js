f76821574fcd3af5810ac8317b354236ffa0020e
akicho8 <akicho8@gmail.com>
2023-01-31 00:02:47 +0900

    [fix] [test] ResizeObserver のせいでテストが通らないのでモックを追加する

--- test/setup.js
+++ test/setup.js
@@ -3,3 +3,10 @@ import Buefy from 'buefy'
 
 Vue.use(Buefy)
 Vue.config.productionTip = false
+
+// https://stackoverflow.com/questions/68679993/referenceerror-resizeobserver-is-not-defined
+window.ResizeObserver = window.ResizeObserver || jest.fn().mockImplementation(() => ({
+  disconnect: jest.fn(),
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+}))
