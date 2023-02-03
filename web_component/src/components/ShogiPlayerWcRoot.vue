4b4f2b542b21900486af4645fb98d3615690f3a0
akicho8 <akicho8@gmail.com>
2023-02-03 09:07:52 +0900

    [from now] 2023/02/03 09:07:52

--- web_component/src/components/ShogiPlayerWcRoot.vue
+++ web_component/src/components/ShogiPlayerWcRoot.vue
@@ -1,18 +1,24 @@
 <template lang="pug">
 .ShogiPlayerWcRoot(part="__root__")
   // CSS変数の初期値はここに定義する
-  // __root__ は実験のため
-  .ShogiPlayerCssVariablesOverride(part="sp_css_variables")
+  // __root__ は確認のためにつけているだけなので使うな
+  .ShogiPlayerCssVariablesOverride1(part="sp_css_variables")
     // 上書きする場合のCSS変数は shogi-player-wc::part(sp_css_variables) {} でここに定義してもらう
     // ShogiPlayerWcRoot でも上書きできたが優先度が曖昧なため階層化している
-    ShogiPlayer(
-      v-bind="$props"
-      v-on="$listeners"
-      @update:sp_turn="e => $emit('update:sp_turn', e)"
-    )
+    .ShogiPlayerCssVariablesOverride2(:style="sp_css_variables_hash")
+      // ここに定義すると shogi-player-wc::part(sp_css_variables) {} に勝つ
+      // 例: sp_css_variables="{'--sp_controller_width':0.8}"
+      ShogiPlayer(
+        v-bind="$props"
+        v-on="$listeners"
+        @update:sp_turn="e => $emit('update:sp_turn', e)"
+      )
 </template>
 
 <script>
+import _ from "lodash"
+import JSON5 from "json5"
+
 // Buefy
 // これは全体で使うことになるので本当は index.js で読み込んだ方がよい
 import Vue from "vue"
@@ -31,14 +37,20 @@ export default {
   components: { ShogiPlayer },
   inheritAttrs: false,
   props: {
+    sp_css_variables: { type: String, },
+
     sp_layout:     { type: String, },
     sp_slider:     { type: String, },
     sp_controller: { type: String, },
     sp_body:       { type: String, },
   },
   mounted() {
+    // console.log(JSON5.parse(""))
+
+    console.log("NODE_ENV", process.env.NODE_ENV)
     console.log("$attrs", this.$attrs)
     console.log("$props", this.$props)
+    console.log(this.sp_css_variables_hash)
   },
   methods: {
     foo(v) {
@@ -56,11 +68,17 @@ export default {
 
     },
   },
+  computed: {
+    sp_css_variables_hash() {
+      return JSON5.parse(this.sp_css_variables ?? "{}")
+    },
+  },
 }
 </script>
 
 <style lang="sass">
-// Shadow DOM 内で読み込まないと届かない
+// Shadow DOM 内で読み込むと i タグに適用されるが「×」の状態になる
+// どこが問題なのかよくわかっていないが外側でもさらに読み込むと「×」が解消される
 @import url("https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css")
 
 // ShogiPlayer のスタイルを読み込む
