961cd250697a85c207eebcef55c6d809f68364f4
akicho8 <akicho8@gmail.com>
2023-02-25 20:49:32 +0900

    [feat] [wc] [buefy] $primary 色を変更する

--- web_component/src/components/ShogiPlayerWcRoot.vue
+++ web_component/src/components/ShogiPlayerWcRoot.vue
@@ -194,26 +194,21 @@ export default {
 </script>
 
 <style lang="sass">
-// Shadow DOM 内で読み込むと i タグに適用されるが「×」の状態になる
-// どこが問題なのかよくわかっていないが外側でもさらに読み込むと「×」が解消される
+// Shadow DOM 内で読み込むとフォントが読み込まれないがその他の部分は適用する必要があるため読み込む
 @import url("https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css")
 
-// @import "@mdi/font/css/materialdesignicons.min.css"
-
-// ShogiPlayer のスタイルを読み込む
-//
-// この場所から assets への相対パスを指定する
-// ShogiPlayer.sass はそれを見て assets の場所がわかる
-// $sp_assets_dir: "shogi-player/assets"
-// $sp_css_root: ".ShogiPlayerWcRoot2"
-// // // // Web Components にした場合 html 内に定義しても shadow-root の中に届かない
-// // // // なので Web Components 内に初期値を適用する
+// ShogiPlayer のスタイルを読み込む($sp_assets_dir の定義が重要)
 @import "shogi-player/components/ShogiPlayer.sass"
 
-// CSS側で読み込むと Web Components 内に含まれる
-@import "buefy/dist/buefy.css"
+// Bulmaのテーマ色を変更する場合
+@import "~bulma/sass/utilities/initial-variables"
+$primary: $blue
+@import "~bulma"
+@import "~buefy/src/scss/buefy"
+// 変更しない場合
+// @import "buefy/dist/buefy.css"
 
-// このあとで +mobile などを使いたいため
+// このあとで +mobile などを使いたいとき
 // @import "~bulma/sass/utilities/_all"
 
 // .ShogiPlayerWcRoot
