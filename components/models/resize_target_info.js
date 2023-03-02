f3b3767326d615866684f19caad850814446f8a9
akicho8 <akicho8@gmail.com>
2023-03-02 12:56:23 +0900

    [refactor] cosmetic change

--- components/models/resize_target_info.js
+++ components/models/resize_target_info.js
@@ -3,7 +3,7 @@ import { ApplicationMemoryRecord } from "./application_memory_record.js"
 export class ResizeTargetInfo extends ApplicationMemoryRecord {
   static get define() {
     return [
-      { key: "BoardBase",   selector: ".BoardBase",   threshold: 0,   attr_w: "sp_board_w", attr_h: "sp_board_h", },
+      { key: "BoardBase", selector: ".BoardBase", threshold: 0,   attr_w: "sp_board_w", attr_h: "sp_board_h", },
       { key: "BoardCell", selector: ".BoardCell", threshold: 1.5, attr_w: "sp_cell_w",  attr_h: "sp_cell_h",  },
     ]
   }
