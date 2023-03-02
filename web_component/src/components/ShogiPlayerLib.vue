5a5808f3a53c4e3427cd5bb9e0f0fe04ffff7727
akicho8 <akicho8@gmail.com>
2023-03-02 21:58:17 +0900

    [fix] ShogiPlayerLib.vue がビルドできない

--- web_component/src/components/ShogiPlayerLib.vue
+++ web_component/src/components/ShogiPlayerLib.vue
@@ -15,7 +15,6 @@ import ShogiPlayer from "shogi-player/components/ShogiPlayer.vue"
 
 // import { EventList } from "shogi-player/components/models/event_list.js"
 
-
 export default {
   name: "ShogiPlayerLib",
   components: { ShogiPlayer },
@@ -23,24 +22,29 @@ export default {
 </script>
 
 <style lang="sass">
-// Shadow DOM 内で読み込むと i タグに適用されるが「×」の状態になる
-// どこが問題なのかよくわかっていないが外側でもさらに読み込むと「×」が解消される
+@import "shogi-player/assets/sass/application.sass"
+
+// Shadow DOM 内で読み込むとフォントが読み込まれないがその他の部分は適用する必要があるため読み込む
 @import url("https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css")
 
-// @import "@mdi/font/css/materialdesignicons.min.css"
-
-// ShogiPlayer のスタイルを読み込む
+// // Shadow DOM 内で読み込むと i タグに適用されるが「×」の状態になる
+// // どこが問題なのかよくわかっていないが外側でもさらに読み込むと「×」が解消される
+// @import url("https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css")
 //
-// この場所から assets への相対パスを指定する
-// ShogiPlayer.sass はそれを見て assets の場所がわかる
-// $sp_assets_dir: "shogi-player/assets"
-// $sp_css_root: ".ShogiPlayerLib2"
-// // // // Web Components にした場合 html 内に定義しても shadow-root の中に届かない
-// // // // なので Web Components 内に初期値を適用する
-@import "shogi-player/components/ShogiPlayer.sass"
-
-// CSS側で読み込むと Web Components 内に含まれる
-@import "buefy/dist/buefy.css"
+// // @import "@mdi/font/css/materialdesignicons.min.css"
+//
+// // ShogiPlayer のスタイルを読み込む
+// //
+// // この場所から assets への相対パスを指定する
+// // ShogiPlayer.sass はそれを見て assets の場所がわかる
+// // $sp_assets_dir: "shogi-player/assets"
+// // $sp_css_root: ".ShogiPlayerLib2"
+// // // // // Web Components にした場合 html 内に定義しても shadow-root の中に届かない
+// // // // // なので Web Components 内に初期値を適用する
+// @import "shogi-player/components/ShogiPlayer.sass"
+//
+// // CSS側で読み込むと Web Components 内に含まれる
+// @import "buefy/dist/buefy.css"
 
 // このあとで +mobile などを使いたいため
 // @import "~bulma/sass/utilities/_all"
