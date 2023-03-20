579f79b8d2177ea49a47b08314030005f13e9a49
akicho8 <akicho8@gmail.com>
2023-03-20 14:23:38 +0900

    [fix] vue-test に依存した jest の設定を除去する

--- jest.config.js
+++ jest.config.js
@@ -6,12 +6,10 @@ module.exports = {
   },
   moduleFileExtensions: [
     'js',
-    'vue',
     'json',
   ],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
   },
   setupFiles: ['<rootDir>/test/setup'],
 }
