7b1d495df1eb2d50e3fb2fee92eec4305db6e0d5
akicho8 <akicho8@gmail.com>
2026-02-10 20:24:26 +0900

    [refactor] [BREAKING_CHANGES] sp_request_snapshot_hash の初期値を false にする

--- components/mod_snapshot_hash.js
+++ components/mod_snapshot_hash.js
@@ -1,5 +1,5 @@
 // ・千日手の判定をするには、アプリ側で snapshot_hash の登場個数をカウントする
-// ・連続王手の千日手の反則判定するには、それに加えて op_king_check を考慮してカウントする
+// ・連続王手の千日手の反則検知するには、それに加えて op_king_check を考慮してカウントする
 // ・shogi-player 側では具体的な実装はせず、単に低レベルの情報だけをイベントで返すようにする
 
 import _ from "lodash"
@@ -7,7 +7,7 @@ import { GX } from "./models/gx"
 
 export const mod_snapshot_hash = {
   props: {
-    sp_request_snapshot_hash: { type: Boolean, default: true,  }, // 操作モードで千日手判定用に現局面のSFENをイベントに含めるか？ (本当はデフォルトを false にしたいが互換性のため true にしている)
+    sp_request_snapshot_hash: { type: Boolean, default: false, }, // 操作モードで千日手判定用に現局面のハッシュをイベントに含めるか？
     sp_request_op_king_check: { type: Boolean, default: false, }, // 操作モードで王手しているかどうかの結果をイベントに含めるか？
   },
   methods: {
