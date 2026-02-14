f32bf752b5645ba96920e497610b357ff6d9381b
akicho8 <akicho8@gmail.com>
2026-02-14 20:44:40 +0900

    [refactor] focus_on_input_tag_p を共通化する

---
+++ components/models/dom_helper.js
@@ -0,0 +1,13 @@
+export const DomHelper = {
+  focus_on_input_tag_p() {
+    const dom = document.activeElement
+    if (dom == null) {
+      return false
+    }
+
+    const is_input = ["INPUT", "TEXTAREA", "SELECT"].includes(dom.tagName)
+    const is_editable = dom.isContentEditable // contenteditable="true" 対策
+
+    return is_input || is_editable
+  },
+}
