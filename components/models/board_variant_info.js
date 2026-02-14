f8cb6da3c3772728cb303b41a38fd4117afcd54e
akicho8 <akicho8@gmail.com>
2026-02-14 20:39:44 +0900

    [feat] 盤テクスチャの種類にSVGエフェクトをいくつか追加する

--- components/models/board_variant_info.js
+++ components/models/board_variant_info.js
@@ -3,14 +3,13 @@ import { ApplicationMemoryRecord } from "./application_memory_record.js"
 export class BoardVariantInfo extends ApplicationMemoryRecord {
   static get define() {
     return [
-      { key: "none",   name: "none",        },
+      { key: "none",        name: "none",        },
       { key: "wood_normal", name: "普通の木目",  },
       { key: "wood_bright", name: "明るい木目",  },
+      { key: "zarazara",    name: "ざらざら",    },
+      { key: "wood_effect",      name: "木目",        },
+      { key: "bump_effect",      name: "エンボス",    },
+      { key: "washi",       name: "和紙",        },
     ]
   }
-}
-
-if (typeof process !== "undefined" && process.argv[1] === __filename) {
-  console.log(BoardVariantInfo.fetch("wood_normal").key)
-  console.log(BoardVariantInfo.fetch("wood_normal").name)
 }
