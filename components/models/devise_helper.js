27c3f385e0108a33ebe1378c4da2ccfb14731ff8
akicho8 <akicho8@gmail.com>
2026-02-18 00:14:30 +0900

    [fix] DeviseHelper.mouse_click_event_p が呼べない

--- components/models/devise_helper.js
+++ components/models/devise_helper.js
@@ -3,7 +3,7 @@ export class DeviseHelper {
   // 本当は pointerType === "mouse" だけでいい
   // しかし iPad の Google Chrome と iPad の Safari でも pointerType === "mouse" になってしまう
   // したがって maxTouchPoints でタッチ可能なデバイスならマウスではないとする
-  mouse_click_event_p(e) {
+  static mouse_click_event_p(e) {
     if (e) {
       const touch_capable_p = navigator.maxTouchPoints > 0
       return e.pointerType === "mouse" && !touch_capable_p
